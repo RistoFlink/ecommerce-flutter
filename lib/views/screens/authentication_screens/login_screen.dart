955579f07631d9dfa907f05c91dfa2d0587c477c
RistoFlink <kenssi@gmail.com>
2025-02-18 14:38:27 +0200

    Finish creating a basic login screen

--- lib/views/screens/authentication_screens/login_screen.dart
+++ lib/views/screens/authentication_screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,6 +7,38 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(body: Column(children: [Text('Login to your account')]));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login to your account',
+              style: GoogleFonts.getFont(
+                'Lato',
+                color: Color(0xFF0d120E),
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.2,
+                fontSize: 23,
+              ),
+            ),
+            Text(
+              'Explore world exclusives',
+              style: GoogleFonts.getFont(
+                'Lato',
+                color: Color(0xFF0d120E),
+                fontSize: 14,
+                letterSpacing: 0.2,
+              ),
+            ),
+            Image.asset(
+              'assets/images/Illustration.png',
+              width: 200,
+              height: 200,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
